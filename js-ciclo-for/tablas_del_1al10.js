ed3e7f573ed7fc57a4fed3bd326c80076fa994a1
PauliMejia <paulimata.1128@gmail.com>
2024-06-17 22:13:30 -0600

    Add files via upload

---
+++ js-ciclo-for/tablas_del_1al10.js
@@ -0,0 +1,8 @@
+for (let multiplicando  = 1; multiplicando <=10; multiplicando++){
+    console.log('tabla de multiplicacion ${multiplicando}:');
+    for(let multiplicador =1; multiplicador <=10; multiplicador++){
+ const res = multiplicando*multiplicador;
+ console.log('${multiplicando} x ${multiplicador} = ${res}'); 
+ }
+
+ }
